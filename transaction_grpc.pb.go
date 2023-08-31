b7190ef9ef566bb7a32c715436fca14ded62b916
Sona <sonasivasundari@gmail.com>
2023-08-31 15:29:03 +0530

    change the service

--- transaction_grpc.pb.go
+++ transaction_grpc.pb.go
@@ -14,86 +14,86 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// CustomerServiceClient is the client API for CustomerService service.
+// TransactionServiceClient is the client API for TransactionService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type CustomerServiceClient interface {
+type TransactionServiceClient interface {
 	TransferMoney(ctx context.Context, in *TransactionData, opts ...grpc.CallOption) (*TransactionResponse, error)
 }
 
-type customerServiceClient struct {
+type transactionServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewCustomerServiceClient(cc grpc.ClientConnInterface) CustomerServiceClient {
-	return &customerServiceClient{cc}
+func NewTransactionServiceClient(cc grpc.ClientConnInterface) TransactionServiceClient {
+	return &transactionServiceClient{cc}
 }
 
-func (c *customerServiceClient) TransferMoney(ctx context.Context, in *TransactionData, opts ...grpc.CallOption) (*TransactionResponse, error) {
+func (c *transactionServiceClient) TransferMoney(ctx context.Context, in *TransactionData, opts ...grpc.CallOption) (*TransactionResponse, error) {
 	out := new(TransactionResponse)
-	err := c.cc.Invoke(ctx, "/netxd_customer.CustomerService/TransferMoney", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/netxd_customer.TransactionService/TransferMoney", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// CustomerServiceServer is the server API for CustomerService service.
-// All implementations must embed UnimplementedCustomerServiceServer
+// TransactionServiceServer is the server API for TransactionService service.
+// All implementations must embed UnimplementedTransactionServiceServer
 // for forward compatibility
-type CustomerServiceServer interface {
+type TransactionServiceServer interface {
 	TransferMoney(context.Context, *TransactionData) (*TransactionResponse, error)
-	mustEmbedUnimplementedCustomerServiceServer()
+	mustEmbedUnimplementedTransactionServiceServer()
 }
 
-// UnimplementedCustomerServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedCustomerServiceServer struct {
+// UnimplementedTransactionServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedTransactionServiceServer struct {
 }
 
-func (UnimplementedCustomerServiceServer) TransferMoney(context.Context, *TransactionData) (*TransactionResponse, error) {
+func (UnimplementedTransactionServiceServer) TransferMoney(context.Context, *TransactionData) (*TransactionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method TransferMoney not implemented")
 }
-func (UnimplementedCustomerServiceServer) mustEmbedUnimplementedCustomerServiceServer() {}
+func (UnimplementedTransactionServiceServer) mustEmbedUnimplementedTransactionServiceServer() {}
 
-// UnsafeCustomerServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to CustomerServiceServer will
+// UnsafeTransactionServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to TransactionServiceServer will
 // result in compilation errors.
-type UnsafeCustomerServiceServer interface {
-	mustEmbedUnimplementedCustomerServiceServer()
+type UnsafeTransactionServiceServer interface {
+	mustEmbedUnimplementedTransactionServiceServer()
 }
 
-func RegisterCustomerServiceServer(s grpc.ServiceRegistrar, srv CustomerServiceServer) {
-	s.RegisterService(&CustomerService_ServiceDesc, srv)
+func RegisterTransactionServiceServer(s grpc.ServiceRegistrar, srv TransactionServiceServer) {
+	s.RegisterService(&TransactionService_ServiceDesc, srv)
 }
 
-func _CustomerService_TransferMoney_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TransactionService_TransferMoney_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(TransactionData)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(CustomerServiceServer).TransferMoney(ctx, in)
+		return srv.(TransactionServiceServer).TransferMoney(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/netxd_customer.CustomerService/TransferMoney",
+		FullMethod: "/netxd_customer.TransactionService/TransferMoney",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(CustomerServiceServer).TransferMoney(ctx, req.(*TransactionData))
+		return srv.(TransactionServiceServer).TransferMoney(ctx, req.(*TransactionData))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// CustomerService_ServiceDesc is the grpc.ServiceDesc for CustomerService service.
+// TransactionService_ServiceDesc is the grpc.ServiceDesc for TransactionService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var CustomerService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "netxd_customer.CustomerService",
-	HandlerType: (*CustomerServiceServer)(nil),
+var TransactionService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "netxd_customer.TransactionService",
+	HandlerType: (*TransactionServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "TransferMoney",
-			Handler:    _CustomerService_TransferMoney_Handler,
+			Handler:    _TransactionService_TransferMoney_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
